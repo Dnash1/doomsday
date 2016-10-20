cc35b4daae147033ef3d386eaadddbe5c9ebbb31
David Nash <david.allyxanddar.nash@gmail.com>
2016-10-20 08:34:51 -0400

    tum te tum

--- public/main.js
+++ public/main.js
@@ -14,7 +14,9 @@ $(window).load(function() {
 		tile_enable_icons: false,
 		tile_enable_image_effect: true,
 		tile_image_effect_type: "bw",
-		tile_enable_textpanel: true
+		tile_enable_textpanel: true,
+		lightbox_show_numbers: false,
+		lightbox_textpanel_enable_description: true
 	});
 	$('.parallax-window').parallax({imageSrc: './img/cover.jpg'});
 	$('.parallax-window2').parallax({imageSrc: './img/2.jpg'});
